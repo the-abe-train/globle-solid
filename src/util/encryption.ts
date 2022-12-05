e5ccc2b7d41fa20d55c8938c8e2425f86c4aed7b
the-abe-train <abraham.train@gmail.com>
2022-12-04 20:59:13 -0500

    Changes

--- src/util/encryption.ts
+++ src/util/encryption.ts
@@ -1,7 +1,7 @@
+import rawAnswerData from "../data/country_data.json";
 import crypto from "crypto-js";
 import dayjs from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
-import { getPracticeAns } from "./practice";
 dayjs.extend(advancedFormat);
 
 const key = import.meta.env.VITE_CRYPTO_KEY;
@@ -16,14 +16,16 @@ export const getDayCode = () => dayjs().endOf("day").format("X");
 
 export async function getAnswer() {
   const today = dayjs().format("YYYY-MM-DD");
+  const listLength = rawAnswerData["features"].length;
   // const endpoint = `/.netlify/functions/answer?day=${today}`;
-  const endpoint = `/answer?day=${today}`;
+  const endpoint = `/answer?day=${today}&list=${listLength}`;
   try {
     const response = await fetch(endpoint);
     if (response.status !== 200) throw "Server error";
-    const data = (await response.json()) as any;
+    const data = (await response.json()) as { answer: string };
     const encryptedAnswer = data.answer;
-    const answer = JSON.parse(decrypt(encryptedAnswer)) as Country;
+    const answerKey = parseInt(decrypt(encryptedAnswer));
+    const answer = rawAnswerData["features"][answerKey] as Country;
     return answer;
   } catch (e) {
     console.error(e);
