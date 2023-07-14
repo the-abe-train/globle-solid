32e092f3dad8ddafb262001fa9f8f3194681a736
Abe Train <abraham.train@gmail.com>
2023-07-14 13:52:56 -0400

    Added ads.txt to vite PWA thing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import solidPlugin from "vite-plugin-solid";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        navigateFallbackDenylist: [/^\/ads\.txt/],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
