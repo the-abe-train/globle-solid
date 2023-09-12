5886f90f9dde9649a29dc21a25f8f461296592eb
Abe Train <abraham.train@gmail.com>
2023-09-12 11:56:09 -0400

    Lebanon and Israel border

--- src/util/geometry.ts
+++ src/util/geometry.ts
@@ -64,6 +64,7 @@ export function polygonDistance(country1: Country, country2: Country) {
   const adjacentCountries: [string, string, number][] = [
     ["South Africa", "Lesotho", 0],
     ["Jordan", "Israel", 0],
+    ["Lebanon", "Israel", 0],
     ["Italy", "Vatican", 0],
     ["Italy", "Vatican City", 0],
     ["Italy", "San Marino", 0],
