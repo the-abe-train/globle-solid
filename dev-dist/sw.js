ccf62a8eccee97876e38dede3dceddbfc8c9e796
Abe Train <47761569+the-abe-train@users.noreply.github.com>
2025-06-24 09:37:50 -0400

    Update service worker revision number and add alternate name for United States in Spanish

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rr6a9jl10a"
+    "revision": "0.ult0uv2cuh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
